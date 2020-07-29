154757e3aef5e126b2247489b1fe9bca87437e7e
trevopoli <trevopoli@gmail.com>
2020-07-29 14:02:28 -0700

    Adding initialize for PolyTreeNode

--- intro_data_structures_polytreenode/lib/00_tree_node.rb
+++ intro_data_structures_polytreenode/lib/00_tree_node.rb
@@ -1,3 +1,14 @@
 class PolyTreeNode
 
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    
+
+
 end
