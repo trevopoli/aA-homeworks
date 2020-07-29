55f3b6a56202aba2b0c63f0ecda39ccf9ccf0728
trevopoli <trevopoli@gmail.com>
2020-07-29 11:57:42 -0700

    Implementing Stack methods

--- DIY_ADTs/stack.rb
+++ DIY_ADTs/stack.rb
@@ -1,17 +1,23 @@
 class Stack
     def initialize
-      # create ivar to store stack here!
+        @stack = []
     end
 
     def push(el)
-      # adds an element to the stack
+      @stack.push(el)
     end
 
     def pop
-      # removes one element from the stack
+      @stack.pop
     end
 
     def peek
-      # returns, but doesn't remove, the top element in the stack
+      @stack[-1]
     end
 end
+
+# stack = Stack.new
+# stack.push("cool")
+# stack.push("stuff")
+# p stack.peek
+# p stack
