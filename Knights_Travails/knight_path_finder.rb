e7d30523a9aa39cb593b71d28bd83b8cb6e0eddf
trevopoli <trevopoli@gmail.com>
2020-08-02 11:19:50 -0700

    fixing build_tree to not repeat

--- Knights_Travails/knight_path_finder.rb
+++ Knights_Travails/knight_path_finder.rb
@@ -1,9 +1,10 @@
 require_relative "poly_tree_node"
+require "byebug"
 
 class KnightPathFinder
 
-    def self.root_node
-        PolyTreeNode.New(@start_pos)
+    def self.root_node(start_pos)
+        PolyTreeNode.new(start_pos)
     end
 
     def self.valid_moves(pos)
@@ -21,23 +22,35 @@ class KnightPathFinder
 
     def initialize(start_pos)
         @start_pos = start_pos
-        build_move_tree(self.root_node)
         @considered_positions = [@start_pos]
+        build_move_tree(KnightPathFinder.root_node(@start_pos))
     end
 
     # def find_path
         
     # end
 
-    def build_move_tree
-        
+    def build_move_tree(root)
+        queue = [root]
+        until queue.empty?
+            pos_node = queue.shift 
+            new_move_positions(pos_node.value).each do |new_pos| #added value to get [x,y] from node
+                new_pos_node = PolyTreeNode.new(new_pos)
+                pos_node.add_child(new_pos_node)
+                queue << new_pos_node
+            end
+        end
     end
 
     def new_move_positions(pos)
-        new_moves = self.valid_moves(pos).select {|move| !@considered_positions.include?(move)}
-        @considered_positions << new_moves
+        new_moves = KnightPathFinder.valid_moves(pos)
+        non_repeat_new_moves = new_moves.select {|move| !@considered_positions.include?(move)}
+        non_repeat_new_moves.each do |ele|
+            @considered_positions << ele
+        end
+        non_repeat_new_moves
     end
 
 end
 
-p KnightPathFinder.valid_moves([0,0])
+kpf = KnightPathFinder.new([0,0])
