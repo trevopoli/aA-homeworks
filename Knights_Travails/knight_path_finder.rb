d385fccdef94022ccdf9b047a683bbe727c9b447
trevopoli <trevopoli@gmail.com>
2020-08-02 11:31:44 -0700

    Adding find_path and trace_path_back

--- Knights_Travails/knight_path_finder.rb
+++ Knights_Travails/knight_path_finder.rb
@@ -23,12 +23,9 @@ class KnightPathFinder
     def initialize(start_pos)
         @start_pos = start_pos
         @considered_positions = [@start_pos]
-        build_move_tree(KnightPathFinder.root_node(@start_pos))
+        @root = KnightPathFinder.root_node(@start_pos)
+        build_move_tree(@root)
     end
-
-    # def find_path
-        
-    # end
 
     def build_move_tree(root)
         queue = [root]
@@ -51,6 +48,24 @@ class KnightPathFinder
         non_repeat_new_moves
     end
 
+    def find_path(end_pos)
+        end_pos_node = @root.bfs(end_pos)
+        trace_path_back(end_pos_node).reverse
+    end
+
+    def trace_path_back(end_pos_node)
+        parent_value = end_pos_node.value
+        path_back = [parent_value]
+        new_node = end_pos_node.parent
+        until new_node == nil
+            path_back << new_node.value
+            new_node = new_node.parent
+        end
+        path_back
+    end
+
 end
 
-kpf = KnightPathFinder.new([0,0])
+# kpf = KnightPathFinder.new([0, 0])
+# p kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+# p kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
