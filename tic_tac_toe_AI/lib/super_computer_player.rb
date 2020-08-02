d434b3557d3a30da1c0ebe405574958b09a76f21
trevopoli <trevopoli@gmail.com>
2020-08-02 15:27:24 -0700

    Adding super_computer_player

--- tic_tac_toe_AI/lib/super_computer_player.rb
+++ tic_tac_toe_AI/lib/super_computer_player.rb
@@ -2,6 +2,19 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    node_children = node.children
+
+    node_children.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+    end
+
+    #no_winning_moves
+    node_children.each do |child|
+      return child.prev_move_pos if !child.losing_node?(mark)
+    end
+
+    raise "No winning or tying move"
   end
 end
 
