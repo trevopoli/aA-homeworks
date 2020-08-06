90a126c67a760741a5d06fadc2b0d13fc6ab4056
trevopoli <trevopoli@gmail.com>
2020-08-05 22:44:36 -0700

    adding turn method for board

--- mancala/lib/board.rb
+++ mancala/lib/board.rb
@@ -1,17 +1,59 @@
 class Board
   attr_accessor :cups
+  attr_reader :name1, :name2
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14)
+    self.place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    (0..13).each do |x|
+      if x == 6 || x == 13
+        @cups[x] = []
+      else
+        @cups[x] = Array.new(4) {:stone}
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    if start_pos == 6 || start_pos < 0 || start_pos > 12
+      raise ArgumentError.new "Invalid starting cup" 
+    end
+
+    raise ArgumentError.new "Starting cup is empty" if @cups[start_pos].length <= 0
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+    i = 1
+    until stones.empty? do
+      next_cup = (start_pos + i) % 14 
+      if current_player_name == @name1
+        @cups[next_cup] << stones.shift if next_cup != 13
+      else
+        @cups[next_cup] << stones.shift if next_cup != 6
+      end   
+      i += 1
+    end
+    self.render
+    self.next_turn(next_cup)
+    
+    case next_cup
+    when 6
+      return :prompt if current_player_name == @name1
+    when 13
+      return :prompt if current_player_name == @name2
+    end
+
+    return :switch if @cups[next_cup].length == 1 # ended on empty cup
+
+    next_cup
   end
 
   def next_turn(ending_cup_idx)
@@ -32,3 +74,6 @@ class Board
   def winner
   end
 end
+
+# board = Board.new('name1', 'name2')
+# p board.cups
