b98e18894d10a1f2bcbab7c580167a9b85e7a11a
trevopoli <trevopoli@gmail.com>
2020-08-06 21:33:50 -0700

    Phase 1 completed

--- chess/board.rb
+++ chess/board.rb
@@ -9,7 +9,8 @@ class Board
             if row == 0 || row == 1 || row == 6 || row == 7
                 @rows[row].map! {|peice| Piece.new}
             else
-                @rows[row].map! {|peice| NullPiece.new}
+                # @rows[row].map! {|piece| NullPiece.new}
+                @rows[row].map! {|piece| nil}
             end
         end
     end
@@ -17,29 +18,39 @@ class Board
     def [](pos)
         raise 'invalid pos' unless valid_pos?(pos)
 
-        row, col = pos
+        row = pos[0]
+        col = pos[1]
+        
         @rows[row][col]
     end
 
     def []=(pos, piece)
         raise 'invalid pos' unless valid_pos?(pos)
 
-        @rows[pos] = piece
+        row = pos[0]
+        col = pos[1]
+        
+        @rows[row][col] = piece
     end
 
     def valid_pos?(pos)
-
+        pos.all? { |i| i.between?(0, 7) }
     end
 
     def move_piece(start_pos, end_pos)
-        raise 'no piece at start_pos' if start_pos.empty?
-
+        raise 'no piece at start_pos' if self.empty?(start_pos)
+        raise 'not a valid end_pos' if !self.valid_pos?(end_pos)
+        
+        self[end_pos] = self[start_pos]
+        self[start_pos] = nil
     end
 
     def empty?(pos)
-        @rows[pos].is_a?(Piece)
+        !self[pos].is_a?(Piece)
     end
 
 end
 
-rows = Board.new
+board = Board.new
+board.move_piece([1,1], [4,4])
+p board
