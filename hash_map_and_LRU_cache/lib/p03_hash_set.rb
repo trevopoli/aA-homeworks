2624a5dc4e6d2b3fddbfdb53216fb70a91b81d9c
trevopoli <trevopoli@gmail.com>
2020-08-25 19:40:00 -0700

    HashSet implemented

--- hash_map_and_LRU_cache/lib/p03_hash_set.rb
+++ hash_map_and_LRU_cache/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,18 +7,29 @@ class HashSet
   end
 
   def insert(key)
+    if !self.include?(key)
+      self[key] << key
+      @count += 1
+    end
+
+    resize! if @count > num_buckets
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    start_size = self[key].length
+    self[key].reject! {|el| el == key}
+    @count -= 1 if start_size != self[key].length
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +37,10 @@ class HashSet
   end
 
   def resize!
+    old_store = self.store
+    @count = 0
+    @store = Array.new(2*num_buckets) {Array.new}   
+
+    old_store.flatten.each {|num| self.insert(num)}
   end
 end
