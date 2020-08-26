18e909b31c27deea51712097a5f0ec0ee89a398f
trevopoli <trevopoli@gmail.com>
2020-08-25 20:46:25 -0700

    Implement LinkedList class

--- hash_map_and_LRU_cache/lib/p04_linked_list.rb
+++ hash_map_and_LRU_cache/lib/p04_linked_list.rb
@@ -16,11 +16,19 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    self.next.prev = self.prev 
+    self.prev.next = self.next 
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +37,65 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    self.each do |node|
+      return true if node.key == key
+    end
+
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    new_node.next = @tail
+    new_node.prev = @tail.prev
+    @tail.prev.next = new_node
+    @tail.prev = new_node
   end
 
   def update(key, val)
+    self.each do |node|
+      node.val = val if node.key == key
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        node.remove
+        return
+      end
+    end
   end
 
   def each
+    next_node = @head.next
+    until next_node.next == nil
+      yield next_node
+      next_node = next_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
