b3dc471dbbae664490ffa7b255c0ac69816a7513
trevopoli <trevopoli@gmail.com>
2020-08-25 19:09:50 -0700

    implementing hash for Array, String, Hash

--- hash_map_and_LRU_cache/lib/p02_hashing.rb
+++ hash_map_and_LRU_cache/lib/p02_hashing.rb
@@ -4,11 +4,15 @@ end
 
 class Array
   def hash
+    self.each_with_index.inject(0) do |ind_hash, (el, i)|
+      (el.hash + i.hash) ^ ind_hash
+    end
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +20,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort_by(&:hash).hash
   end
 end
