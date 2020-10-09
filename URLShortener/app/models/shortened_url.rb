9f90c96d48bb4564c115064e645442ce65083b6b
trevopoli <trevopoli@gmail.com>
2020-10-08 20:03:02 -0700

    ShortenedUrl model and table

---
+++ URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,20 @@
+class ShortenedUrl < ApplicationRecord
+    validates :short_url, uniqueness: true, presence: true
+    validates :long_url, presence: true
+    validates :user_id, presence: true
+
+    def self.random_code
+        loop do
+            random_code = SecureRandom.urlsafe_base64(16)
+            return random_code unless ShortenedUrl.exists?(short_url: random_code)
+        end
+    end
+
+    def self.create_for_user_and_long_url!(user, long_url)
+        ShortenedUrl.create!(
+            user_id: user.id,
+            long_url: long_url,
+            short_url: ShortenedUrl.random_code
+        )
+    end
+end
