40b5fd51a31e4950b0dc1340bbda17a95806681d
trevopoli <trevopoli@gmail.com>
2020-10-26 18:06:35 -0700

    migration for TagTopics

--- URLShortener/db/schema.rb
+++ URLShortener/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_035139) do
+ActiveRecord::Schema.define(version: 2020_10_27_010419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2020_10_09_035139) do
     t.datetime "updated_at", null: false
     t.index ["short_url"], name: "index_shortened_urls_on_short_url", unique: true
     t.index ["user_id"], name: "index_shortened_urls_on_user_id"
+  end
+
+  create_table "tag_topics", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
