ee94c9c928a835b885aac56edff27ee7f4ef490f
trevopoli <trevopoli@gmail.com>
2020-11-16 20:54:44 -0700

    02_searchable completed

--- byo_activerecord/lib/02_searchable.rb
+++ byo_activerecord/lib/02_searchable.rb
@@ -3,10 +3,23 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.map {|key, value| " #{key} = ? "}.join("AND")
+    values = params.values
+
+    results = DBConnection.execute(<<-SQL, *values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    self.parse_all(results)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
